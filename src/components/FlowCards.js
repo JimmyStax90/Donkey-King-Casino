ab6d3272b5ad2eca98eddce3fa84f420d9584341
Yasir <yasirzinbox@gmail.com>
2022-08-12 19:04:45 +0500

    changed class to className

--- src/components/FlowCards.js
+++ src/components/FlowCards.js
@@ -43,48 +43,48 @@ const FlowCards = () => {
                     </div>
                 </div>
             </div>
-			<div class="row flow_sha d-md-none">
-                <div class="col-lg-12">
-                    <div class="game_flow">
-                        {/* <div class="row flow_slider"> */}
+			<div className="row flow_sha d-md-none">
+                <div className="col-lg-12">
+                    <div className="game_flow">
+                        {/* <div className="row flow_slider"> */}
 						<Slider {...settings}>
-                            <div class="col-lg-12 border_shadow">
-                                <div class="game_text">
+                            <div className="col-lg-12 border_shadow">
+                                <div className="game_text">
                                     <h2><span>0</span><span>1</span></h2>
                                     <h3>Register</h3>
                                     <p>Create Your Account</p>
                                 </div>
                             </div>
-                            <div class="col-lg-12 border_shadow">
-                                <div class="game_text">
+                            <div className="col-lg-12 border_shadow">
+                                <div className="game_text">
                                     <h2>02</h2>
                                     <h3>Connect Your Wallet</h3>
                                     <p>Buy Casino Chips w/ DonkSwap Token ($DST)</p>
                                 </div>
                             </div>
-                            <div class="col-lg-12 border_shadow">
-                                <div class="game_text">
+                            <div className="col-lg-12 border_shadow">
+                                <div className="game_text">
                                     <h2>03</h2>
                                     <h3>Enjoy</h3>
                                     <p>Play Your Favorite Casino Games on the web or on your phone/tablet</p>
                                 </div>
                             </div>
-                            <div class="col-lg-12 border_shadow">
-                                <div class="game_text">
+                            <div className="col-lg-12 border_shadow">
+                                <div className="game_text">
                                     <h2><span>0</span><span>1</span></h2>
                                     <h3>Register</h3>
                                     <p>Create Your Account</p>
                                 </div>
                             </div>
-                            <div class="col-lg-12 border_shadow">
-                                <div class="game_text">
+                            <div className="col-lg-12 border_shadow">
+                                <div className="game_text">
                                     <h2>03</h2>
                                     <h3>Enjoy</h3>
                                     <p>Play Your Favorite Casino Games on the web or on your phone/tablet</p>
                                 </div>
                             </div>
-                            <div class="col-lg-12 border_shadow">
-                                <div class="game_text">
+                            <div className="col-lg-12 border_shadow">
+                                <div className="game_text">
                                     <h2>03</h2>
                                     <h3>Enjoy</h3>
                                     <p>Play Your Favorite Casino Games on the web or on your phone/tablet</p>
