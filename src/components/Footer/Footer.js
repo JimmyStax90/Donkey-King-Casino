6a7709ada54f4ed4ece465b26be353cd8b9e8056
Jimmy Stax <87676344+JimmyStax90@users.noreply.github.com>
2022-11-03 14:06:27 -0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,11 +26,11 @@ const Footer = () =>{
                         <div className="col-md-7">
                             <div className="foot_info_text">
                                 <ul>
-                                    <li><a href="#/">Live Score</a></li>
+                                    <li><a href="https://www.donkeykingeg.com/404/">Live Score</a></li>
                                     <li><a href="https://docs.donkeykingeg.com/how-to-play/">How to Play</a></li>
-                                    <li><a href="#/">Tournaments</a></li>
-                                    <li><a href="#/">Registration Bonus</a></li>
-                                    <li><a href="marketing@donkswap.com/">Contact</a></li>
+                                    <li><a href="https://www.donkeykingeg.com/404/">Tournaments</a></li>
+                                    <li><a href="https://www.donkeykingeg.com/404/">Registration Bonus</a></li>
+                                    <li><a href="https://www.donkeykingeg.com/404/">Contact</a></li>
                                 </ul>
                             </div>
                         </div>
