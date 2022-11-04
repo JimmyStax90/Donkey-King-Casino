5114a6e7e5b9db5c7561601fa1221cf6b80516b5
CoconutMage <coconutmage@gmail.com>
2022-11-04 13:37:52 -0500

    Update Slots.js

--- src/pages/Slots.js
+++ src/pages/Slots.js
@@ -12,9 +12,31 @@ function Slots() {
   });
   document.body.style.backgroundColor = "rgb(44, 31, 56)";
 	return (
-		<div>
-			<Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
-		</div>
+		
+`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="logo.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <link rel="apple-touch-icon" href="logo.png" />
+    
+    <link rel="manifest" href="/manifest.json" />
+    
+    <title>Donkey King Casino</title>
+  <script defer src="/static/js/bundle.js"></script></head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    
+  </body>
+</html>`
+
 	);
 }
 
