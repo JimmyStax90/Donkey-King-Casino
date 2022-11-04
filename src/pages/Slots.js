3835bf013def3a41971df3ff419295a4c6d49724
CoconutMage <coconutmage@gmail.com>
2022-11-04 14:20:17 -0500

    Update Slots.js

--- src/pages/Slots.js
+++ src/pages/Slots.js
@@ -1,17 +1,44 @@
-function iframe() {
-    return {
-        __html: '<iframe src="keno/index.html" width="540" height="450"></iframe>'
-    }
-}
+import React from 'react';
+import MetaTags from 'react-meta-tags';
+import { Link } from 'react-router-dom';
+import Navigation from './navbar/Navigation';
+import Footer from 'components/Footer/Footer';
+import PaymentMethod from 'components/PaymentMethod';
 
 
-function Slots() {
-	console.log("this haps");
-	document.body.style.backgroundColor = "rgb(44, 31, 56)";
+const Slots = () => {
     return (
         <div>
-            <div dangerouslySetInnerHTML={iframe()} />
-        </div>)
-}
+            <MetaTags>
+                <title>404 | Donkey King Casino</title>
+                <meta name="description" content="Donkey King Casino" />
+                <meta property="og:title" content="Donkey King Casino" />
+                <meta property="og:image" content="../logo.png" />
+          </MetaTags>
+            <Navigation/>
+            {/* Error 404 part start */}
+            <section id="claim_part" className="client_bg client_bg2">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                            <div className="error_page_main">
+                                <img src="images/DKEG Logo No BG.png" className="img-fluid" alt="08_404_Page"/>
+                                <div className="error_btn">
+                                    <Link to="/">Go Back to Home</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Error 404 part end */}
+            {/* Payment part Start */}
+            <PaymentMethod/>
+            {/* Payment part end */}
+            {/* Footer Part */}
+            <Footer/>
+        </div>
+    );
+};
 
 export default Slots;
