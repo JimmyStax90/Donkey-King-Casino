55030e3cec8daaa24f8779594ebb9e358669e323
CoconutMage <coconutmage@gmail.com>
2022-11-04 15:00:20 -0500

    Update Slots.js

--- src/pages/Slots.js
+++ src/pages/Slots.js
@@ -1,44 +1,19 @@
-import React from 'react';
-import MetaTags from 'react-meta-tags';
-import { Link } from 'react-router-dom';
-import Navigation from './navbar/Navigation';
-import Footer from 'components/Footer/Footer';
-import PaymentMethod from 'components/PaymentMethod';
+import React from "react";
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
-
-const Slots = () => {
-    return (
-        <div>
-            <MetaTags>
-                <title>404 | Donkey King Casino</title>
-                <meta name="description" content="Donkey King Casino" />
-                <meta property="og:title" content="Donkey King Casino" />
-                <meta property="og:image" content="../logo.png" />
-          </MetaTags>
-            <Navigation/>
-            {/* Error 404 part start */}
-            <section id="claim_part" className="client_bg client_bg2">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 text-center">
-                            <div className="error_page_main">
-                                <img src="images/DKEG Logo No BG.png" className="img-fluid" alt="08_404_Page"/>
-                                <div className="error_btn">
-                                    <Link to="/">Go Back to Home</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* Error 404 part end */}
-            {/* Payment part Start */}
-            <PaymentMethod/>
-            {/* Payment part end */}
-            {/* Footer Part */}
-            <Footer/>
-        </div>
-    );
-};
+function Slots() {
+	const { unityProvider } = useUnityContext({
+    loaderUrl: "unityFiles/SlotsGZ.loader.js",
+    dataUrl: "unityFiles/SlotsGZ.data",
+    frameworkUrl: "unityFiles/SlotsGZ.framework.js",
+    codeUrl: "unityFiles/SlotsGZ.wasm",
+  });
+  document.body.style.backgroundColor = "rgb(44, 31, 56)";
+	return (
+		<div>
+			<Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+		</div>
+	);
+}
 
 export default Slots;
