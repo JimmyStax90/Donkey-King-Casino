36da722c538fea8947a8dcb8284f54de1f21b326
CoconutMage <coconutmage@gmail.com>
2022-11-04 13:32:28 -0500

    Update Slots.js

--- src/pages/Slots.js
+++ src/pages/Slots.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { Unity, useUnityContext } from 'react-unity-webgl';
+
+
 
 function Slots() {
-	
+	const { unityProvider } = useUnityContext({
+    loaderUrl: "unityFiles/SlotsGZ.loader.js",
+    dataUrl: "unityFiles/SlotsGZ.data",
+    frameworkUrl: "unityFiles/SlotsGZ.framework.js",
+    codeUrl: "unityFiles/SlotsGZ.wasm",
+  });
   document.body.style.backgroundColor = "rgb(44, 31, 56)";
-	
+	return (
+		<div>
+			<Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+		</div>
+	);
 }
 
 export default Slots;
