751dd36585782b078f5d5c31fe1854a22d6df57c
Jimmy Stax <87676344+JimmyStax90@users.noreply.github.com>
2022-11-06 17:02:46 -0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -50,7 +50,7 @@ const Footer = () =>{
                         <h3>Security Info</h3>
                         <div className="foot_secu_text">
                             <div className="">
-                                <img src="images/foot_icon1.png" alt="foot_icon1"/>
+                                <img src="images/BSC Logo - No BG.png" alt="foot_icon1"/>
                             </div>
                             <div className="">
                                 <p>Safe &amp; Secure <span>Crypto Gaming</span></p>
