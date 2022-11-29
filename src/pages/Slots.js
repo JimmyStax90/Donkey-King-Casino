3a35b9d3d438d7645d8f62955946dc2b9efe0948
CoconutMage <coconutmage@gmail.com>
2022-11-29 15:38:53 -0600

    Added Keno and back Buttons

--- src/pages/Slots.js
+++ src/pages/Slots.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { Unity, useUnityContext } from 'react-unity-webgl';
+import Navigation from './navbar/NavigationGame';
+import MetaTags from 'react-meta-tags';
+import { useNavigate } from 'react-router-dom';
 
 function Slots() {
-	const { unityProvider } = useUnityContext({
+	const navigate = useNavigate();
+	const { unityProvider, unload } = useUnityContext({
     loaderUrl: "unityFiles/SlotsGZ.loader.js",
     dataUrl: "unityFiles/SlotsGZ.data",
     frameworkUrl: "unityFiles/SlotsGZ.framework.js",
     codeUrl: "unityFiles/SlotsGZ.wasm",
   });
-  document.body.style.backgroundColor = "rgb(44, 31, 56)";
+  async function handleClickBack() {
+    await unload();
+	window.history.go(-1);
+    // Ready to navigate to another page.
+  }
+  document.body.style.backgroundColor = "rgb(62, 3, 158)";
 	return (
 		<div>
+			<div><button onClick={handleClickBack}>Back</button></div>
 			<Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
 		</div>
 	);
